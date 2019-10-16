104bb78998a7eb5389d8a704a4a89f8a9f1f21d8
DenisChervonyy <chervonyy.denis@gmail.com>
2019-10-16 20:59:10 +0300

    merged custom-select-test into master

--- app/components/statistic/statistic.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('StatisticModule')
-    .controller('StatisticController', function ($scope, $routeParams) {
-        $scope.welcome = 'This is the statistic page!';
-        $scope.idParam = $routeParams.id;
-    });
